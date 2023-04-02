a53e43113663827ee84d4e8363349c0a13285854
NoRapport <contact@norapportradio.net>
2023-04-02 02:24:01 +0200

    Update LevelManager.cs

--- Assets/Scripts/GameMechanics/LevelManager.cs
+++ Assets/Scripts/GameMechanics/LevelManager.cs
@@ -35,7 +35,6 @@ public class LevelManager : MonoBehaviour
       if(Input.GetKeyDown(KeyCode.L))
       {
         level+=1;
-        Debug.Log(level);
       }
 
       if(Input.GetKeyDown(KeyCode.M))
